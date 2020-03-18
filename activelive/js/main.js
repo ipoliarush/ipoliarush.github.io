bb5a101bebd9f6c9839275d448d413e85c6ece9b
Ivan Poliarush <56694325+ipoliarush@users.noreply.github.com>
2020-03-18 12:49:41 +0200

    Update main.js

--- activelive/js/main.js
+++ activelive/js/main.js
@@ -1,1 +1,22 @@
-
+$('.parallaxie').parallaxie({
+  speed: 0.4,
+  size : "auto",
+});
+    
+var wow = new WOW(
+  {
+    offset: 100,
+    mobile: false,
+  }
+);
+wow.init();
+    
+document.body.onload = function() {
+  setTimeout(function() {
+    var preloader = document.getElementById('loader');
+    if( !preloader.classList.contains('done') )
+    {
+      preloader.classList.add('done');
+    }
+  }, 1000)
+}
